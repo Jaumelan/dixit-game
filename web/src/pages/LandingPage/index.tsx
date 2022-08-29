8feb493325305db0ca1d73f68fdbf7cc1a1c678f
Fernando <ribeiro.fer50@gmail.com>
2022-08-29 18:29:04 -0300

    landingPage

--- web/src/pages/LandingPage/index.tsx
+++ web/src/pages/LandingPage/index.tsx
@@ -1,7 +1,34 @@
+import "./style.css"
+
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
+
+    <div id="page">
+      <nav>
+        <ul>
+          <li><button id="home"></button></li>
+          <li><button id="gameRule"></button></li>
+        </ul>
+        <ul id="rightNav">
+          <li><button id="ptbr"></button></li>
+          <li><button id="login">LOGIN</button></li>
+        </ul>
+      </nav>
+
+      <div id="mainmain">
+        <div id="main">
+          <div id="boxLeft">
+            <img src="../../static/img/gameName.png" alt="" />
+            <button id="signUp">Cadastre-se</button>
+          </div>
+
+          <div id="boxRight">
+            <img src="../../static/img/gameLogo.png" alt="" />
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 };

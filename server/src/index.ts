963c6b6bd288ef921d2379769b7e335a37e81dd2
washington Gacho <washington.gacho@dcomp.ufs.br>
2022-09-06 10:08:52 -0300

    cors backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,5 @@
 import { app } from './server';
 import { config } from './config';
-
 app.listen(config.port, () => {
   console.log(`Server is listening on port ${config.port}`);
 });
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
+
+const apCors = cors({ origin: '*' });
 
 const app = express();
+app.use(apCors);
 app.use(routes);
-
 export { app };

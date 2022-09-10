a1bcf6454a14a00e761efe69f5155d8ee0747fbc
ivantonial <ivantonial@gmail.com>
2022-09-10 14:11:47 -0300

    search game layout correction

--- web/src/components/EnterGameSession/styles.ts
+++ web/src/components/EnterGameSession/styles.ts
@@ -17,17 +17,17 @@ export const Container = styled.div`
   padding: 1rem 2rem;
 
   position: relative;
-  border-radius: 10px;
+  border-radius: 20px;
   -webkit-animation: fade-appear-in 2s cubic-bezier(0.39, 0.575, 0.565, 1) both;
   animation: fade-appear-in 2s cubic-bezier(0.39, 0.575, 0.565, 1) both;
   position: relative;
-  border: 3px solid #000;
+  border: 3px solid #D3D3D3;
   gap: 1rem;
 `;
 
 export const TitleContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
   width: 100%;
 `;
@@ -42,6 +42,7 @@ export const Title = styled.h1`
 export const SessionContainer = styled.div`
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: center;
   gap: 1rem;
@@ -54,13 +55,22 @@ export const SessionEnter = styled.div`
   justify-content: center;
   background: #c0c0c0;
   border-radius: 10px;
+  border: 2px solid #000;
   width: 15rem;
   padding: 1rem 2rem;
   gap: 1rem;
 
   h3 {
-    font-size: 1.5rem;
+    font-size: 2rem;
     font-weight: 700;
     color: #000;
   }
+
+  button {
+    margin-top: -1.5rem;
+    border: 2px solid #000;
+    border-radius: 10px;
+    width: 1rem;
+  }
 `;
+

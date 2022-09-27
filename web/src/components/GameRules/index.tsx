fa50031f4c7bf39c82b5a191a7fbf382d1c6ee8d
Jaime <jaume.lan@gmail.com>
2022-09-27 18:44:40 -0300

    fix: gamerules

--- web/src/components/GameRules/index.tsx
+++ web/src/components/GameRules/index.tsx
@@ -6,6 +6,107 @@ import Typography from "@mui/material/Typography";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import * as S from "./styles";
 
+/*
+type GameRulesProps = {
+  closeGameRules: (d: boolean) => void;
+};
+
+const GameRules: FC<GameRulesProps> = ({ closeGameRules }) => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") closeGameRules(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    
+  }, [value]);
+
+  return (
+    <S.Container>
+      <S.TabContainer>
+        <S.Tabs>
+          <S.TabName>
+            <S.Name value={0} onClick={handleChange}>
+              Regras do Jogo
+            </S.Name>
+          </S.TabName>
+
+          <S.Name value={1} onClick={handleChange}>
+            Pontuação
+          </S.Name>
+        </S.Tabs>
+        <AiOutlineCloseCircle
+          size={29}
+          onClick={() => closeGameRules(false)}
+          style={{ cursor: "pointer" }}
+          color="#004080"
+        />
+      </S.TabContainer>
+
+      <S.TabContent>
+        {value === 0 && (
+          <S.RulesContainer id="rules">
+            <h3>Regras do Jogo</h3>
+            <span>
+              Cada Jogador recebe 5 Cartas aleatórias. A cada rodada teremos um
+              jogador que será o contador de histórias. O contador de histórias
+              tem a função de selecionar uma de suas cartas e descrever essa
+              carta em uma palavra ou frase.
+            </span>
+            <span>
+              A descrição não deve ser clara o suficiente para que todos os
+              outros jogadores acertem a carta escolhida pelo contador de
+              histórias, nem vaga demais, de forma que nenhum outro jogador
+              acerte.
+            </span>
+            <span>
+              Após o contador de histórias realizar sua jogada, os demais
+              jogadores devem escolher uma carta que melhor se assemelhe com a
+              descrição da carta do contador de histórias. Após todos terem
+              jogado, os jogadores devem tentar adivinhar qual é a carta do
+              contador de histórias.
+            </span>
+          </S.RulesContainer>
+        )}
+        {value === 1 && (
+          <S.RulesContainer id="about">
+            <h3>Pontuação</h3>
+            <span>
+              Cada Jogador recebe 5 Cartas aleatórias. A cada rodada teremos um
+              jogador que será o contador de histórias. O contador de histórias
+              tem a função de selecionar uma de suas cartas e descrever essa
+              carta em uma palavra ou frase.
+            </span>
+            <span>
+              A descrição não deve ser clara o suficiente para que todos os
+              outros jogadores acertem a carta escolhida pelo contador de
+              histórias, nem vaga demais, de forma que nenhum outro jogador
+              acerte.
+            </span>
+            <span>
+              Após o contador de histórias realizar sua jogada, os demais
+              jogadores devem escolher uma carta que melhor se assemelhe com a
+              descrição da carta do contador de histórias. Após todos terem
+              jogado, os jogadores devem tentar adivinhar qual é a carta do
+              contador de histórias.
+            </span>
+          </S.RulesContainer>
+        )}
+      </S.TabContent>
+    </S.Container>
+  );
+};
+
+export default GameRules;
+*/
+
 type TabPanelProps = {
   children?: React.ReactNode;
   index: number;
@@ -92,55 +193,68 @@ const GameRules: FC<GameRulesProps> = ({ closeGameRules }) => {
             color="#000040"
           />
         </Box>
-        <TabPanel value={value} index={0}>s
-an          <S.RulesContainer>
-            <h3>Regras do Jogo</h3>
-            <span>
-              Cada Jogador recebe 5 Cartas aleatórias. A cada rodada teremos um
-              jogador que será o contador de histórias. O contador de histórias
-              tem a função de selecionar uma de suas cartas e descrever essa
-              carta em uma palavra ou frase.{" "}
-            </span>
-            <span>
-              A descrição não deve ser clara o suficiente para que todos os
-              outros jogadores acertem a carta escolhida pelo contador de
-              histórias, nem vaga demais, de forma que nenhum outro jogador
-              acerte.{" "}
-            </span>
-            <span>
-              {" "}
-              Após o contador de histórias realizar sua jogada, os demais
-              jogadores devem escolher uma carta que melhor se assemelhe com a
-              descrição da carta do contador de histórias. Após todos terem
-              jogado, os jogadores devem tentar adivinhar qual é a carta do
-              contador de histórias.
-            </span>
-          </S.RulesContainer>
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <S.Pontos>
-            <h3>PONTUAÇÃO</h3>
-
-            <span>
-              Caso todos jogadores ou nenhum deles acertem a carta do contador
-              de histórias, este não receberá nenhum ponto e todos os demais
-              recebem 2 pontos.
-            </span>
-            <span>
-              Caso o contador de história receba ao menos um voto mas não todos,
-              ele receberá 3 pontos.
-            </span>
-            <span>
-              Os jogadores que escolheram a carta correta também ganham 3
-              pontos.
-            </span>
-            <span>
-              Os jogadores, com excessão ao contador de histórias, que tiveram
-              sua carta escolhidas pelos demais jogadores ganham 1 ponto para
-              cada escolha.
-            </span>
-          </S.Pontos>
-        </TabPanel>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+          {value === 0 && (
+            <>
+              <Typography
+                variant="h3"
+                component="h3"
+                gutterBottom
+                sx={{ color: "#004080", alignContent: "center", marginTop: "1rem", }}
+              >
+                Regras do Jogo
+              </Typography>
+              <Typography variant="h6" component="h6" gutterBottom>
+                Cada Jogador recebe 5 Cartas aleatórias. A cada rodada teremos
+                um jogador que será o contador de histórias. O contador de
+                histórias tem a função de selecionar uma de suas cartas e
+                descrever essa carta em uma palavra ou frase.{" "}
+              </Typography>
+              <Typography variant="h6" component="h6" gutterBottom>
+                A descrição não deve ser clara o suficiente para que todos os
+                outros jogadores acertem a carta escolhida pelo contador de
+                histórias, nem vaga demais, de forma que nenhum outro jogador
+                acerte. {}
+              </Typography>
+              <Typography variant="h6" component="h6" gutterBottom>
+                Após o contador de histórias realizar sua jogada, os demais
+                jogadores devem escolher uma carta que melhor se assemelhe com a
+                descrição da carta do contador de histórias. Após todos terem
+                jogado, os jogadores devem tentar adivinhar qual é a carta do
+                contador de histórias.
+              </Typography>
+            </>
+          )}
+          {value === 1 && (
+            <>
+              <Typography
+                variant="h3"
+                component="h3"
+                gutterBottom
+                sx={{
+                  color: "#004080",
+                  alignContent: "center",
+                  marginTop: "1rem",
+                }}
+              >
+                Pontuação
+              </Typography>
+              <Typography variant="h6" component="h6" gutterBottom>
+                Caso todos jogadores ou nenhum deles acertem a carta do contador
+                de histórias, este não receberá nenhum ponto e todos os demais
+                recebem 2 pontos.
+              </Typography>
+              <Typography variant="h6" component="h6" gutterBottom>
+                Caso o contador de história receba ao menos um voto mas não
+                todos, ele receberá 3 pontos.
+              </Typography>
+              <Typography variant="h6" component="h6" gutterBottom>
+                Os jogadores que escolheram a carta correta também ganham 3
+                pontos.
+              </Typography>
+            </>
+          )}
+        </Box>
       </Box>
     </S.Container>
   );
--- web/src/components/GameRules/styles.ts
+++ web/src/components/GameRules/styles.ts
@@ -23,6 +23,135 @@ export const Container = styled.div`
   }
 `;
 
+export const TabContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 6rem;
+  padding: 0rem 2rem;
+  position: relative;
+  z-index: 1;
+  -webkit-animation: fade-appear-in 1s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+  animation: fade-appear-in 1s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+  gap: 1rem;
+  border-bottom: 1px solid #004080;
+`;
+
+export const Tabs = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 6rem;
+  padding: 0rem 2rem;
+  
+  -webkit-animation: fade-appear-in 1s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+  animation: fade-appear-in 1s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+  gap: 1rem;
+  
+  }
+  
+`;
+
+
+export const TabName = styled.div`
+  transition: 0.2s;
+  
+  
+  heigth: 6rem;
+  &:hover {
+    transition: all 1s;
+    background: rgba(0, 0, 0, 0.1);
+    border-left: 1px solid #004080;
+    border-right: 1px solid #004080;
+  }
+
+  &.active {
+    transition: all 1s;
+    background: rgba(0, 0, 0, 0.1);
+    border-left: 1px solid #004080;
+    border-right: 1px solid #004080;
+  }
+`;
+
+export const Name = styled.button`
+  font-size: 2.1rem;
+  font-weight: 600;
+  font-family: "Roboto", sans-serif;
+  color: #004080;
+  cursor: pointer;
+  transition: 0.2s;
+  border: none;
+  line-height: 5.5rem;
+  background: none;
+  heigth: 6rem;
+  &:hover {
+    transition: all 1s;
+    background: rgba(0, 0, 0, 0.1);
+    border-left: 1px solid #004080;
+    border-right: 1px solid #004080;
+  }
+`;
+
+export const TabContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+  gap: 1rem;
+  overflow-y: auto;
+  overflow-x: hidden;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const RulesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h3 {
+    font-size: 2rem;
+    line-height: 2.2rem;
+    color: #000040;
+  }
+  span {
+    font-size: 1.5rem;
+    diplay: inline-block;
+    margin-top: 1rem;
+  }
+`;
+
+export const Pontos = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h3 {
+    font-size: 2rem;
+    line-height: 2.2rem;
+    color: #000040;
+  }
+  span {
+    font-size: 1.5rem;
+    align-self: flex-start;
+    display: inline-block;
+    margin-top: 1rem;
+  }
+`;
+
+/*
 export const RulesContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -55,3 +184,4 @@ export const Pontos = styled.div`
 
   
 `;
+*/

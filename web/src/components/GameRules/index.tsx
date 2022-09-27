8d5c12023c3ae5a1e208b8ef2f4854ea2672a91d
Jaime <jaume.lan@gmail.com>
2022-09-27 18:46:26 -0300

    fix: gamerules

--- web/src/components/GameRules/index.tsx
+++ web/src/components/GameRules/index.tsx
@@ -5,7 +5,6 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import * as S from "./styles";
-
 type TabPanelProps = {
   children?: React.ReactNode;
   index: number;
@@ -92,55 +91,68 @@ const GameRules: FC<GameRulesProps> = ({ closeGameRules }) => {
             color="#000040"
           />
         </Box>
-        <TabPanel value={value} index={0}>s
-an          <S.RulesContainer>
-            <h3>Regras do Jogo</h3>
-            <span>
-              Cada Jogador recebe 5 Cartas aleatórias. A cada rodada teremos um
-              jogador que será o contador de histórias. O contador de histórias
-              tem a função de selecionar uma de suas cartas e descrever essa
-              carta em uma palavra ou frase.{" "}
-            </span>
-            <span>
-              A descrição não deve ser clara o suficiente para que todos os
-              outros jogadores acertem a carta escolhida pelo contador de
-              histórias, nem vaga demais, de forma que nenhum outro jogador
-              acerte.{" "}
-            </span>
-            <span>
-              {" "}
-              Após o contador de histórias realizar sua jogada, os demais
-              jogadores devem escolher uma carta que melhor se assemelhe com a
-              descrição da carta do contador de histórias. Após todos terem
-              jogado, os jogadores devem tentar adivinhar qual é a carta do
-              contador de histórias.
-            </span>
-          </S.RulesContainer>
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <S.Pontos>
-            <h3>PONTUAÇÃO</h3>
-
-            <span>
-              Caso todos jogadores ou nenhum deles acertem a carta do contador
-              de histórias, este não receberá nenhum ponto e todos os demais
-              recebem 2 pontos.
-            </span>
-            <span>
-              Caso o contador de história receba ao menos um voto mas não todos,
-              ele receberá 3 pontos.
-            </span>
-            <span>
-              Os jogadores que escolheram a carta correta também ganham 3
-              pontos.
-            </span>
-            <span>
-              Os jogadores, com excessão ao contador de histórias, que tiveram
-              sua carta escolhidas pelos demais jogadores ganham 1 ponto para
-              cada escolha.
-            </span>
-          </S.Pontos>
-        </TabPanel>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+          {value === 0 && (
+            <>
+              <Typography
+                variant="h3"
+                component="h3"
+                gutterBottom
+                sx={{ color: "#004080", alignContent: "center", marginTop: "1rem", }}
+              >
+                Regras do Jogo
+              </Typography>
+              <Typography variant="h6" component="h6" gutterBottom>
+                Cada Jogador recebe 5 Cartas aleatórias. A cada rodada teremos
+                um jogador que será o contador de histórias. O contador de
+                histórias tem a função de selecionar uma de suas cartas e
+                descrever essa carta em uma palavra ou frase.{" "}
+              </Typography>
+              <Typography variant="h6" component="h6" gutterBottom>
+                A descrição não deve ser clara o suficiente para que todos os
+                outros jogadores acertem a carta escolhida pelo contador de
+                histórias, nem vaga demais, de forma que nenhum outro jogador
+                acerte. {}
+              </Typography>
+              <Typography variant="h6" component="h6" gutterBottom>
+                Após o contador de histórias realizar sua jogada, os demais
+                jogadores devem escolher uma carta que melhor se assemelhe com a
+                descrição da carta do contador de histórias. Após todos terem
+                jogado, os jogadores devem tentar adivinhar qual é a carta do
+                contador de histórias.
+              </Typography>
+            </>
+          )}
+          {value === 1 && (
+            <>
+              <Typography
+                variant="h3"
+                component="h3"
+                gutterBottom
+                sx={{
+                  color: "#004080",
+                  alignContent: "center",
+                  marginTop: "1rem",
+                }}
+              >
+                Pontuação
+              </Typography>
+              <Typography variant="h6" component="h6" gutterBottom>
+                Caso todos jogadores ou nenhum deles acertem a carta do contador
+                de histórias, este não receberá nenhum ponto e todos os demais
+                recebem 2 pontos.
+              </Typography>
+              <Typography variant="h6" component="h6" gutterBottom>
+                Caso o contador de história receba ao menos um voto mas não
+                todos, ele receberá 3 pontos.
+              </Typography>
+              <Typography variant="h6" component="h6" gutterBottom>
+                Os jogadores que escolheram a carta correta também ganham 3
+                pontos.
+              </Typography>
+            </>
+          )}
+        </Box>
       </Box>
     </S.Container>
   );
